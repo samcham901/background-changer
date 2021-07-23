a56ea3012f9146ba4e2d7be1f3eb55ccb9cdd840
samcham901 <87777327+samcham901@users.noreply.github.com>
2021-07-22 18:58:09 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box_red;
+var box_green
+function setup() {
+  createCanvas(400,400);
+  
+  box_red = createSprite(100,200,50,30)
+  box_red = createSprite(100,200,50,30)
+}
+
+function draw() 
+{
+  background(50);
+//if(Mouse)
+
+  drawSprites();
+}
+
+
+
+
